05a268104819b6c166a7a5dfccf5eb03d09e25a5
Marcus Forsberg <marcus@winter.ax>
2020-01-09 21:02:47 +0200

    Ignore lib

--- lib/express.d.ts
+++
@@ -1,4 +0,0 @@
-import { Application } from '@feathersjs/feathers';
-import { SamlSetupSettings } from './utils';
-declare const _default: (options: SamlSetupSettings) => (feathersApp: Application<{}>) => void;
-export default _default;
--- lib/utils.d.ts
+++
@@ -1,5 +0,0 @@
-import { Application } from '@feathersjs/feathers';
-export interface SamlSetupSettings {
-    authService?: string;
-}
-export declare const getDefaultSettings: (_app: Application<{}>, other?: Partial<SamlSetupSettings>) => SamlSetupSettings;
